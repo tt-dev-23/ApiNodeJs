a447d7ee6d0c2615dfa4677f7707bab5317c89ec
DenisArger <den.arger@gmail.com>
2023-04-11 16:02:41 +0300

    Добавлены методы добавлены данных и получения по id

--- app.js
+++ app.js
@@ -6,33 +6,73 @@ const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabaseApp = supabase.createClient(supabaseUrl, supabaseKey);
 
-async function addUser(name) {
-  try {
-    const { error } = await supabaseApp
-      .from("user")
-      .insert({ name: `${name}` });
+// async function addUser(name) {
+//   try {
+//     const { error } = await supabaseApp
+//       .from("user")
+//       .insert({ name: `${name}` });
 
-    if (error) throw error;
-  } catch (e) {
-    console.log(e);
+//     if (error) throw error;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// async function getCountUser() {
+//   try {
+//     const { error, count } = await supabaseApp
+//       .from("user")
+//       .select("id", { count: "exact", head: true });
+//     if (error) throw error;
+//     return { count };
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// async function getTask() {
+//   try {
+//     const { data, error } = await supabaseApp.from("user").select();
+//     if (error) throw error;
+//     return data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// addUser("Petr");
+
+// getTask().then((res) => console.log(res));
+// getCount().then((res) => console.log(res));
+
+async function addJsonData(id, data) {
+  try {
+    try {
+      if (typeof data === "object") {
+        const { error } = await supabaseApp
+          .from("jsons_tb")
+          .insert([{ id: id, data: data }]);
+
+        if (error) throw error;
+      } else throw new NotJsonException();
+    } catch (e) {
+      console.log(e);
+    }
+  } catch (NotJsonException) {
+    console.log(NotJsonException.message);
   }
 }
 
-async function getCountUser() {
-  try {
-    const { error, count } = await supabaseApp
-      .from("user")
-      .select("id", { count: "exact", head: true });
-    if (error) throw error;
-    return { count };
-  } catch (e) {
-    console.log(e);
-  }
+function NotJsonException() {
+  this.message = "data is not object";
 }
 
-async function getTask() {
+async function getJsonData(id) {
   try {
-    const { data, error } = await supabaseApp.from("user").select();
+    const { data, error } = await supabaseApp
+      .from("jsons_tb")
+      .select("data")
+      .eq("id", id);
     if (error) throw error;
     return data;
   } catch (e) {
@@ -40,7 +80,12 @@ async function getTask() {
   }
 }
 
-// addUser("Petr");
+let id = "asdcde1-";
+let data = {
+  name: "Denis",
+  age: 32,
+};
 
-getTask().then((res) => console.log(res));
-// getCount().then((res) => console.log(res));
+// addJsonData(id, data);
+
+getJsonData(id).then((res) => console.log(res));
