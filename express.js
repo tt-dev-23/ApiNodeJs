c3ef8e1a474f9575c82586493eb1f145ddc0f554
DenisArger <den.arger@gmail.com>
2023-05-03 14:05:07 +0300

    Добавлена обработка статических файлов

--- express.js
+++ express.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.static("assets"));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
