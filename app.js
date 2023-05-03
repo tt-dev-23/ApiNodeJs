3b0d27b85806d9d313f84b3fcb199788d9826d24
DenisArger <den.arger@gmail.com>
2023-05-03 10:03:32 +0300

    Убрана ненужная информация Селекторы перенесены внутрь функций Добавлена проверка на пустые данныеd textarea

--- app.js
+++ app.js
@@ -24,48 +24,47 @@ const addJsonDataApp = async (dataJson) => {
 window.addEventListener("DOMContentLoaded", () => {
   const getQuery = document.querySelector(".get");
   const postQuery = document.querySelector(".post");
-  const jsonData = document.querySelector(".json_data");
-
-  const idData = document.querySelector("#id");
-
-  const idText = document.querySelector(".IdText");
-  const jsText = document.querySelector(".jsText");
-
-  const postError = document.querySelector(".postError");
-  const getError = document.querySelector(".getError");
-
-  const labelGet = document.querySelector(".labelGet");
-  const labelPost = document.querySelector(".labelPost");
-
-  const jsonDataStr = jsonData.value;
-  const jsonObj = JSON.parse(jsonDataStr);
 
   getQuery.addEventListener("click", () => {
+    const jsText = document.querySelector(".jsText");
+    const labelGet = document.querySelector(".labelGet");
+    const getError = document.querySelector(".getError");
+    const idData = document.querySelector("#id");
+
     getJsonDataApp(idData.value).then((res) => {
       if (res.status == 200) {
-        labelGet.innerHTML = "json:";
-        jsText.innerHTML = JSON.stringify(res.data);
-        getError.innerHTML = "";
+        labelGet.innerText = "json:";
+        jsText.innerText = JSON.stringify(res.data);
+        getError.innerText = "";
       } else {
-        labelGet.innerHTML = "error:";
-        jsText.innerHTML = "";
-        getError.innerHTML = res.data.message;
+        labelGet.innerText = "error:";
+        jsText.innerText = "";
+        getError.innerText = res.data.message;
       }
     });
   });
   postQuery.addEventListener("click", () => {
-    addJsonDataApp(jsonObj).then((res) => {
-      console.log(res.status);
+    const jsonData = document.querySelector(".json_data");
+    const idText = document.querySelector(".IdText");
+    const labelPost = document.querySelector(".labelPost");
+    const postError = document.querySelector(".postError");
 
-      if (res.status == 201) {
-        labelPost.innerHTML = "id:";
-        idText.innerHTML = res.id;
-        postError.innerHTML = "";
-      } else {
-        labelPost.innerHTML = "error:";
-        idText.innerHTML = "";
-        postError.innerHTML = res.message;
-      }
-    });
+    if (jsonData.value.trim() != "") {
+      addJsonDataApp(JSON.parse(jsonData.value)).then((res) => {
+        if (res.status == 201) {
+          labelPost.innerText = "id:";
+          idText.innerText = res.id;
+          postError.innerText = "";
+        } else {
+          labelPost.innerText = "error:";
+          idText.innerText = "";
+          postError.innerText = res.message;
+        }
+      });
+    } else {
+      labelPost.innerText = "error:";
+      idText.innerText = "";
+      postError.innerText = "not data";
+    }
   });
 });
