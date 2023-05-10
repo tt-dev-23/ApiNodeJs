d070d79a79147b6c0be80eff08ce8f9d7ec56d86
DenisArger <den.arger@gmail.com>
2023-05-10 16:45:42 +0300

    Реализован get запрос на локально  машине

---
+++ netlify/functions/get.js
@@ -0,0 +1,30 @@
+const { getJsonData } = require("../../supabase.js");
+
+export const handler = async (event) => {
+  const id = event.queryStringParameters.id;
+  if (id.length !== 6) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "incorrect id",
+      }),
+    };
+  } else {
+    try {
+      const data = await getJsonData(id);
+      console.log(data);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: error.message,
+        }),
+      };
+    }
+  }
+};
