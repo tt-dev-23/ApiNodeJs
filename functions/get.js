2a8789e308d60d0f628cecdaa04d88601fcee6cd
DenisArger <den.arger@gmail.com>
2023-05-12 17:16:02 +0300

    Версия, в которой не работает get запрос в браузер

--- functions/get.js
+++ functions/get.js
@@ -1,7 +1,14 @@
 const { getJsonData } = require("../supabase.js");
 
-export const handler = async (event) => {
+export const handler = async (event, context) => {
+  // const pathSplit = event.path.split("/");
+  // console.log("pathSplit", pathSplit);
+  console.log("query", event);
+  console.log("context", context);
+
   const id = event.queryStringParameters.id;
+  console.log("id", id);
+
   if (id.length !== 6) {
     return {
       statusCode: 400,
