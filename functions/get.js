b8b3c4f6d87de8ea90a477756f5e29154721cbac
DenisArger <den.arger@gmail.com>
2023-05-17 11:45:01 +0300

    Отладка редиректа get функции

--- functions/get.js
+++ functions/get.js
@@ -1,43 +1,48 @@
 const { getJsonData } = require("../supabase.js");
 
-export const handler = async (event, context) => {
-  if (event === undefined)
-    return {
-      statusCode: 405,
-      body: JSON.stringify({
-        message: "event undefined",
-      }),
-    };
-  // const pathSplit = event.path.split("/");
-  // console.log("pathSplit", pathSplit);
-  console.log("query", event);
-  console.log("context", context);
-
-  const id = event.queryStringParameters.id;
-  console.log("id", id);
-
-  if (id.length !== 6) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "incorrect id",
-      }),
-    };
-  } else {
-    try {
-      const data = await getJsonData(id);
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
-    } catch (error) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: error.message,
-        }),
-      };
-    }
-  }
+export const handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
 };
+// if (event === undefined)
+//   return {
+//     statusCode: 405,
+//     body: JSON.stringify({
+//       message: "event undefined",
+//     }),
+//   };
+// // const pathSplit = event.path.split("/");
+// // console.log("pathSplit", pathSplit);
+// console.log("query", event);
+// console.log("context", context);
+
+// const id = event.queryStringParameters.id;
+// console.log("id", id);
+
+// if (id.length !== 6) {
+//   return {
+//     statusCode: 400,
+//     body: JSON.stringify({
+//       message: "incorrect id",
+//     }),
+//   };
+// } else {
+//   try {
+//     const data = await getJsonData(id);
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(data),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({
+//         message: error.message,
+//       }),
+//     };
+//   }
+// }
+// };
